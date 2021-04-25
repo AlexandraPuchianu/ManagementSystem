28d903c7d2d8a7aa9c56b85b14207e72de255b3c
AzureAD\Alexandra-GeorgianaP <alexandra.puchianu@principal33.com>
2021-04-25 19:01:44 +0300

    Add functionality for adding or deleting an employee.

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-function validateInput(){
-    var firstName = document.getElementById('fname').value;
-    var lastName = document.getElementById('lname').value;
-    var email = document.getElementById('email').value;
-
+var person = {firstName:"", 
+            lastName:"", 
+            email:"", 
+            gender:"",
+            birthdate:""
+        };
+        
+function validateInput(firstName, lastName, email){
+        
     if (!firstName || !lastName || !email) {
         return false;
     }
-    
     return true;
+}
+        
+function addEmployee() {
+    let employeesTable = document.querySelector("table");
+
+    person.firstName = document.getElementById('fname').value;
+    person.lastName = document.getElementById('lname').value;
+    person.email = document.getElementById('email').value;
+    
+    if (!validateInput(person.firstName, person.lastName, person.email)) {
+        warning.innerHTML="You need to fill all the information!";
+        return;
+    }
+        
+    warning.innerHTML="";
+
+    person.gender = document.getElementById('gender').value;
+    person.birthdate = document.getElementById('birthdate').value;
+        
+    let newRow = ` <tr>
+                        <td id="firstName">${person.firstName}</td>
+                        <td id="lastName">${person.lastName}</td>
+                        <td id="email">${person.email}</td>
+                        <td id="gender">${person.gender}</td>
+                        <td id="birthdate">${person.birthdate}</td>
+                        <td><button onclick="deleteEmployee(this)" id="deleteBtn"><i class="fa fa-close"></i></button></td>
+                    </tr>`;
+    
+    console.log(employeesTable)
+            
+    employeesTable.innerHTML += newRow;
+}
+
+function deleteEmployee(elem) {
+    var row = elem.parentNode.parentNode;
+    row.parentNode.removeChild(row);
 }
